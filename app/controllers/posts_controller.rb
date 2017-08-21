3115650229f1e6ca18c369a664ec41c5383064c0
Jet Talan <jvtalan1@up.edu.ph>
2017-08-21 15:58:00 +0800

    Adds Posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,57 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :show, :update, :delete]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Post was successfully created."
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "There was an error creating new post."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post was successfully updated."
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "There was an error updating the post."
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if @post.destroy
+      flash[:notice] = "Successfully deleted post."
+      redirect_to posts_path
+    else
+      flash[:alert] = "There was an error deleting the post."
+    end
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
