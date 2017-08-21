b78b92a23d1ac9d3e98429f0a69eabfd1c232725
Jet Talan <jvtalan1@up.edu.ph>
2017-08-21 20:54:53 +0800

    Adds User using devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  root to: "posts#index"
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root "posts#index", as: :authenticated_root
+    end
+
+    unauthenticated do
+      root "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
 
   resources :posts
 end
