4a880d9d845b4725de79d92d90bc4246ab74b549
JV Talan <jvtalan1@up.edu.ph>
2017-08-22 14:51:46 +0800

    Updates post integration test adding user association

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 describe 'Posts Page Navigation', type: :feature do
   describe 'Posts Creation' do
     context 'when user input is valid' do
+      let!(:user) { FactoryGirl.create(:user, email: 'user@test.com', password: 'password') }
+
       before do
-        visit '/posts'
-        click_link 'Create new post' 
+        visit '/users/sign_in'
+        fill_in 'Email', with: 'user@test.com'
+        fill_in 'Password', with: 'password'
+        click_button 'Log in'
+        click_link 'Create new post'
         fill_in 'post_body', with: 'Hello diggle!'
         click_button 'Create Post'
       end
