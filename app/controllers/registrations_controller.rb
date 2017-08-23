f0e62e92a59135a4870b969b9e44dc205d378a0e
JV Talan <jvtalan1@up.edu.ph>
2017-08-23 15:44:02 +0800

    Adds profile to Users

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+
+  def after_sign_up_path_for(resource)
+    edit_user_registration_path(resource)
+  end
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :full_name, :username, :phone_number, :location])
+  end
+
+end
---
+++ db/migrate/20170823062839_add_profile_to_users.rb
@@ -0,0 +1,10 @@
+class AddProfileToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :full_name, :string
+    add_column :users, :username, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :location, :string
+
+    add_index :users, :username, unique: true
+  end
+end
