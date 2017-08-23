8f67df9c6a115b0f751c355698b8fb319c1beb8f
JV Talan <jvtalan1@up.edu.ph>
2017-08-23 15:44:02 +0800

    Adds profile to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170822135319) do
+ActiveRecord::Schema.define(version: 20170823062839) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
@@ -43,8 +43,13 @@ ActiveRecord::Schema.define(version: 20170822135319) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
+    t.string "username"
+    t.string "phone_number"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
