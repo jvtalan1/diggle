3863ac46fcb4ed24274210b906fe14e034a272fe
Jet Talan <jvtalan1@up.edu.ph>
2017-08-23 23:56:04 +0800

    Removes updating of post and modifies post index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,40 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # db/seed.rb
+User.delete_all
+Post.delete_all
+Relationship.delete_all
 
-User.create(
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'users'")
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'posts'")
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'relationships'")
+
+User.create!(
+  full_name: 'User Example',
+  username: 'userexample',
+  phone_number: '0999-999990',
+  location: 'Philippines',
   email: 'user@example.com',
   password: 'password'
 )
 
-10.times do |x|
+20.times do |x|
   name = Faker::HarryPotter.character
   domain = Faker::Internet.domain_name
-  User.create(
+  User.create!(
     full_name: name,
-    username: name.strip.downcase,
+    username: name.gsub(/\s+/, '').downcase + x.to_s,
     phone_number: '0999-99999' + x.to_s,
-    location: Faker::HarryPotter.location,
-    email: name.strip.downcase + '@' + domain,
+    location: Faker::Address.country,
+    email: name.gsub(/\s+/, '').downcase + x.to_s + '@' + domain,
     password: 'password'
   )
+  x = x + 1
 end
 
 users = User.all
 users.each do |user|
-  Post.create(
+  Post.create!(
     body: Faker::HarryPotter.quote,
     user_id: user.id
   )
