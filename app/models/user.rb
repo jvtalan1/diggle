6b891c6b131d287c007a3ddc15f14796d112f4fa
JV Talan <jvtalan1@up.edu.ph>
2017-08-24 17:26:06 +0800

    Adds feature to upload pictures on post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :posts
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships
   mount_uploader :picture, PictureUploader
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824045921) do
+ActiveRecord::Schema.define(version: 20170824074202) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "picture"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
