b46a4aa0c4fd6a5fc8afac69f2a5f51faa277e41
Jet Talan <jvtalan1@up.edu.ph>
2017-08-28 23:44:24 +0800

    Fixes routing issue on static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   def show
-    render "pages/#{params[:page]}"
+    if valid_page?
+      render "pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
+  end
+
+  private
+
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.haml"))
   end
 end
