fb625115db5a112652a0224f57337aaf01ef6246
Jet Talan <jvtalan1@up.edu.ph>
2017-08-28 20:32:42 +0800

    Adds email confirmation upon sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,8 +3,8 @@ class RegistrationsController < Devise::RegistrationsController
   
   protected
 
-  def after_sign_up_path_for(resource)
-    edit_user_registration_path(resource)
+  def update_resource(resource, params)
+    resource.update_without_password(params)
   end
   
   def configure_permitted_parameters
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :username, presence: true, uniqueness: true
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
