9178ae05fd85c6a6d0b5327dd8532df5d989a0f8
JV Talan <jvtalan1@up.edu.ph>
2017-08-30 18:46:16 +0800

    Updates User model spec test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    username 'testuser'
     email 'test@user.com'
     password 'password'
+    after(:build)   { |u| u.skip_confirmation_notification! }
+    after(:create)  { |u| u.confirm }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,6 @@ require 'rails_helper'
 
 describe User do
   describe 'validations' do
-    context 'when username is blank' do
-      let(:user) { FactoryGirl.build(:user, username: '') }
-
-      it 'should not be valid' do
-        expect(user.valid?).to be false
-      end
-
-      it 'returns an error message' do
-        user.save
-        expect(user.errors.count).to eq 1
-      end
-    end
-
     context 'when email is blank' do
       let(:user) { FactoryGirl.build(:user, email: '') }
 
@@ -41,31 +28,47 @@ describe User do
       end
     end
 
-    context 'when username is already taken' do
-      let!(:user) { FactoryGirl.create(:user) }
-      let(:user2) { FactoryGirl.build(:user, username: 'testuser', email: 'test2@user.com') }
+    context 'when password does not meet length criteria' do
+      let(:user) { FactoryGirl.build(:user, password: '1234') }
 
       it 'should not be valid' do
+        expect(user.valid?).to be false
+      end
+
+      it 'returns an error message' do
+        user.save
+        expect(user.errors.count).to eq 1
+      end
+    end
+
+    context 'when email is already taken' do
+      let!(:user) { FactoryGirl.build(:user) }
+      let(:user2) { FactoryGirl.build(:user, email: 'test@user.com') }
+
+      it 'should not be valid' do
+        user.skip_confirmation!
+        user.save!
         expect(user2.valid?).to be false
       end
 
       it 'returns an error message' do
+        user.skip_confirmation!
+        user.save!
         user2.save
         expect(user2.errors.count).to eq 1
       end
     end
 
-    context 'when email is already taken' do
-      let!(:user) { FactoryGirl.create(:user) }
-      let(:user2) { FactoryGirl.build(:user, username: 'testuser2', email: 'test@user.com') }
+    context 'when email is not a valid format' do
+      let(:user) { FactoryGirl.build(:user, email: 'test.com') }
 
       it 'should not be valid' do
-        expect(user2.valid?).to be false
+        expect(user.valid?).to be false
       end
 
       it 'returns an error message' do
-        user2.save
-        expect(user2.errors.count).to eq 1
+        user.save
+        expect(user.errors.count).to eq 1
       end
     end
   end
