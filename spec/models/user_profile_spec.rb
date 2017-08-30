2ce56e7ff0b87721b8a44a14df3f3d5543fb74d3
JV Talan <jvtalan1@up.edu.ph>
2017-08-30 18:48:09 +0800

    Adds UserProfile model test

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe UserProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe UserProfile do
+  describe 'validations' do
+    context 'when username is blank' do
+      let(:user_profile) { FactoryGirl.build(:user_profile, username: '') }
+
+      it 'should not be valid' do
+        expect(user_profile.valid?).to be false
+      end
+
+      it' returns an error message' do
+        user_profile.save
+        expect(user_profile.errors.count).to eq 1
+      end
+    end
+
+    context 'when user is absent' do
+      let(:user_profile) { FactoryGirl.build(:user_profile, user_id: '') }
+
+      it' should not be valid' do
+        expect(user_profile.valid?).to be false
+      end
+
+      it 'returns an error message' do
+        user_profile.save
+        expect(user_profile.errors.count).to eq 2
+      end
+    end
+  end
 end
