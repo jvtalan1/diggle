b0cb90a2ae231f8438cc4e72482ce82300bf54e0
JV Talan <jvtalan1@up.edu.ph>
2017-08-30 18:53:27 +0800

    Updates Posts integration test

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe 'Posts Page Navigation', type: :feature do
   describe 'Posts Creation' do
     context 'when user input is valid' do
-      let!(:user) { FactoryGirl.create(:user, username: 'usertest', email: 'user@test.com', password: 'password') }
+      let!(:user) { FactoryGirl.create(:user, email: 'user@test.com', password: 'password', user_profile_attributes: { username: 'testuser' }) }
 
       before do
         visit '/users/sign_in'
-        fill_in 'Email', with: 'user@test.com'
-        fill_in 'Password', with: 'password'
+        fill_in 'user_email', with: 'user@test.com'
+        fill_in 'user_password', with: 'password'
         click_button 'Log in'
         fill_in 'post_body', with: 'Hello diggle!'
         click_button 'Share'
