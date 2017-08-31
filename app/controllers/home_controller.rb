b106208a827adcc0c5563b9e9047d9df61c4db8f
JV Talan <jvtalan1@up.edu.ph>
2017-08-31 20:46:27 +0800

    Fixes ajax query on posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
+  before_action :fetch_user_following
   before_action :fetch_user_stats, only: [:index]
   before_action :fetch_posts, only: [:index]
   before_action :fetch_unconnected_users, only: [:index]
@@ -10,18 +11,21 @@ class HomeController < ApplicationController
   
   private
 
+  def fetch_user_following
+    @following_ids = current_user.following.ids << current_user.id
+  end
+
   def fetch_posts
-    @ids = current_user.following.ids << current_user.id
-    @posts = Post.where(id: @ids).order('created_at DESC')
+    @posts = Post.where(user_id: @following_ids).order('created_at DESC')
   end
 
   def fetch_user_stats
     @following = current_user.following.count
     @followers = current_user.followers.count
-    @posts_count = Post.where(user_id: current_user).count
+    @posts_count = Post.where(user_id: current_user.id).count
   end
 
   def fetch_unconnected_users
-    @users = User.where.not(id: @ids).shuffle[0..4]
+    @users = User.where.not(id: @following_ids).shuffle[0..4]
   end
 end
